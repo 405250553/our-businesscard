355846fc1228bd8e0d1b2c110e659feb88e41dac
Your Name <ken518chen@gmail.com>
2024-03-09 15:24:46 +0800

    [lcd] support lcd, including add uboot size, add uboot/kernel config, modify dts, modify genimage.cfg dtb name [bug] dts led pin is conflict with lcd, dts include file uboot is not support(lcd only show in kernel message) [file]     new file:   board/businesscard/v1/dts/businesscard-with-lcds.dts     modified:   board/businesscard/v1/dts/businesscard.dts     modified:   board/businesscard/v1/genimage.cfg     modified:   board/businesscard/v1/linux_defconfig     modified:   board/businesscard/v1/post-build.sh     modified:   board/businesscard/v1/uboot_defconfig     new file:   configs/thirtythreeforty_businesscard_lcd_defconfig

--- board/businesscard/v1/post-build.sh
+++ board/businesscard/v1/post-build.sh
@@ -3,3 +3,5 @@ grep -q "GADGET_SERIAL" "${TARGET_DIR}/etc/inittab" \
 	|| echo '/dev/ttyGS0::respawn:/sbin/getty -L  /dev/ttyGS0 0 vt100 # GADGET_SERIAL' >> "${TARGET_DIR}/etc/inittab"
 grep -q "ubi0:persist" "${TARGET_DIR}/etc/fstab" \
 	|| echo 'ubi0:persist	/root		ubifs	defaults	0	0' >> "${TARGET_DIR}/etc/fstab"
+grep -q "tty0::respawn" "${TARGET_DIR}/etc/inittab" \
+	|| echo 'tty0::respawn:-/bin/sh # LCD' >> "${TARGET_DIR}/etc/inittab"
